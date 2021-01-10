b8cef8b2892835d1e1ef6f70feff30d4264c91c7
lakekaka <likewen@foxmail.com>
2021-01-10 13:32:59 +0800

    'fisrtGit'

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lake
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		System.out.print("hello idea!");
+		System.out.print("hello idea! Hello Github");
+	
+	}
+
+}
